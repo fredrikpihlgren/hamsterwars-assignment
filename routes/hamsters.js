2ea373b203ad9c135d265916007f4245a731652e
fredrikpihlgren <70364712+fredrikpihlgren@users.noreply.github.com>
2021-04-23 15:52:44 +0200

    added PUT & DELETE

--- routes/hamsters.js
+++ routes/hamsters.js
@@ -51,7 +51,9 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
     const object = req.body; //OBS! måste installera express.json för att detta ska funka
 
-    if (!object || !object.name || !object.age) {
+    console.log('POST TESTING: ', object);
+
+    if (!isHamsterObject(object)) {
         res.sendStatus(400);
         return;
     }
@@ -62,7 +64,56 @@ router.post('/', async (req, res) => {
 
 
 //PUT /hamsters/:id
+
+//KOLLA PATCH OCKSÅ
+
+router.put('/:id', async (req, res) => {
+    const object = req.body; //OBS! måste installera express.json för att detta ska funka
+    const id = req.params.id;
+
+    if (!object || !id) {
+        res.sendStatus(400);
+        return;
+    }
+
+    /*vi kan kontrollera om det finns ett doc som matchar id i databasen
+    Denna kod godkänner id som inte matchar, och lägger till ett nytt doc i databasen
+    */
+
+    const docRef=db.collection('hamstrar').doc(id);
+    await docRef.set(object, {merge: true});
+    res.sendStatus(200);
+
+});
+
+
+function isHamsterObject(object) {
+    if (!object) {
+        return false;
+    }
+    else if (!object.name || !object.age) {
+        return false;
+    }
+    return true;
+}
+
 //DELETE /hamsters/:id
+
+router.delete('/:id', async (req, res) => {
+    const id=req.params.id;
+
+    if (!id) {
+        res.sendStatus(400);
+        return
+    }
+    if (!docRef.exists) {
+        res.status(404).send('Hamster does not exist');
+        return
+    }
+
+    await db.collection('hamstrar').doc(id).delete();
+    res.sendStatus(200);
+});
 
 
 module.exports = router;
